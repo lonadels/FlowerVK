95a335abcc6bfc077535fb77a0edc6e7061fa625
Lonadels <lonadels@ya.ru>
2018-07-28 18:23:10 +0300

    rebuild 7

--- src/app/modules/Updater.php
+++ src/app/modules/Updater.php
@@ -11,7 +11,7 @@ class Updater
     private $mainModule;
     
     public $ver = 1.4;
-    public $build = 6;
+    public $build = 7;
     
     function __construct($mainModule){
         $this->mainModule = $mainModule;        
